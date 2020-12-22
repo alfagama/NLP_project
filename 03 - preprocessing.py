3f31c76fe94b918835f382e3302e43861f2728ed
Arampatzis Giorgos <48099515+alfagama@users.noreply.github.com>
2020-12-22 14:51:51 +0200

    Add files via upload
    adding 02 - dummy_db.py & 03 - preprocessing.py

---
+++ 03 - preprocessing.py
@@ -0,0 +1,94 @@
+###################################################
+#   Pre-Processing
+###################################################
+#
+# 1. We removed the hashtag symbol and its content (e.g., #COVID19), @users, and URLs from
+# the messages because the hashtag symbols or the URLs did not contribute to the message
+# analysis.
+# 2. We removed all non-English characters (non-ASCII characters) because the study focused
+# on the analysis of messages in English.
+# 3. We removed repeated words. For example, sooooo terrified was converted to so terrified.
+# 4. We removed special characters, punctuations, and numbers from the dataset as they did
+# not help with detecting the profanity comments.
+# ola mikra
+# tokenize
+# stop words remove
+###################################################
+
+
+import pymongo
+from pymongo import MongoClient
+from nltk.corpus import stopwords
+import re
+from nltk.stem.snowball import SnowballStemmer
+
+connection = MongoClient("mongodb://localhost:27017/")
+db = connection.TwitterDB
+
+collections = db.collection_names()
+
+stop_words = set(stopwords.words('english'))
+stemmer = SnowballStemmer("english")
+
+def preprocessing(txt):
+    """
+    :param txt: (string) for preprocessing
+    :return: (string) preprocessed
+    """
+    text_processed = txt
+    tokens = text_processed.split()
+
+    #   We removed the hashtag symbol and its content (e.g., #COVID19), @users, and URLs from the messages because the
+    #       hashtag symbols or the URLs did not contribute to the message analysis.
+    tokens_basic_pre = [token for token in tokens if not token.startswith('http')
+                        if not token.startswith('#') if not token.startswith('@')]
+
+    #   We removed all non-English characters (non-ASCII characters) because the study focused on the analysis of
+    #       messages in English.
+    #   We also removed special characters, punctuations, and numbers.
+    tokens_only_letters = list(filter(lambda ele: re.search("[a-zA-Z\s]+", ele) is not None, tokens_basic_pre))
+
+    #   We removed repeated words. For example, sooooo terrified was converted to so terrified.
+    tokens_no_dragging = tokens_only_letters
+
+    #   We removed stop_words
+    tokens_no_stop_words = [w for w in tokens_no_dragging if not w in stop_words]
+
+    #   We set all words to lower-case
+    tokens_lower_case = [x.lower() for x in tokens_no_stop_words]
+
+    # return a list of tokens without preprocessing and a list of tokens after all preprocessing pipeline
+    return tokens, tokens_lower_case
+
+
+def update_preprocessed_column(tokens, clean_tokens):
+    """
+    :param tokens: import tokenized full_text to import in a new column in db
+    :param clean_tokens: import tokenized & cleaned full_text to import in a new column in db
+    :return: Nones
+    """
+    db[collection].update(
+        {
+            "full_text": tweet["full_text"]
+        },
+        {
+            "$set": {
+                "text_tokenized": tokens,
+                "text_tokenized_without_stopwords": clean_tokens
+            }
+        }
+    )
+
+
+for collection in collections:
+    tweets = db[collection].find().batch_size(10)
+    if collection == 'vaccine_test':
+        for tweet in tweets:
+            text = db[collection].find_one({'full_text': tweet["full_text"]})["full_text"]
+            print("Before: ")
+            print(text)
+            print("After: ")
+            tokens, preprocessed_tokens = preprocessing(text)
+            update_preprocessed_column(tokens, preprocessed_tokens)
+            print(tokens)
+            print(preprocessed_tokens)
