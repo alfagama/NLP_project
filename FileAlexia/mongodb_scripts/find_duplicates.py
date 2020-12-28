7d9dd12af8615e3ed3b6bb008bd7df4c4bae7819
Alexia Fytili <33041542+alexiafy@users.noreply.github.com>
2020-12-28 19:03:16 +0200

    Database management scripts commit

---
+++ FileAlexia/mongodb_scripts/find_duplicates.py
@@ -0,0 +1,26 @@
+from mongodb_scripts.mongo_db import *
+import timeit
+
+start = timeit.default_timer()
+
+coll = db["covid_dupls2"]
+cursor = coll.aggregate(
+    [
+        {"$group": {"_id": "$id", "unique_ids": {"$addToSet": "$_id"}, "count": {"$sum": 1}}},
+        {"$match": {"count": { "$gte": 2 }}}
+    ],
+    allowDiskUse=True
+)
+
+duplicate_found = False
+
+for duplicate in cursor:
+    duplicate_found = True
+    print(duplicate)
+
+if not duplicate_found:
+    print('No duplicates found')
+
+
+stop = timeit.default_timer()
+print('Time: %.5f' % (stop - start))
