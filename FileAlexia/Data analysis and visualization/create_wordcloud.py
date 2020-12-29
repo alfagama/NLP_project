15dc4280c35d87b658b21c6c766b3dcbe7569e2f
Your Name <alexia.fytili@hotmail.gr>
2020-12-29 22:45:59 +0200

    Changes made

--- FileAlexia/Data analysis and visualization/create_wordcloud.py
+++ FileAlexia/Data analysis and visualization/create_wordcloud.py
@@ -1,15 +1,30 @@
-# Import the wordcloud library
 from wordcloud import WordCloud
-from mongo_db import *
+from FileAlexia.mongodb_scripts.mongo_db import *
 import matplotlib.pyplot as plt
+import timeit
 
-collection = db['covidtemp']
-tweet_texts = collection.distinct("text_preprocessed")
-#print(tweet_texts)
 
-#Should we delete keywords such as covid19?!!!!!!!!!!todo
-# Join the different processed titles together.
-long_string = ','.join(list(tweet_texts))
+######################################
+start = timeit.default_timer()
+######################################
+
+collection = db['covidtemp2']
+
+cursor = collection.aggregate([#{"$limit": 12000},
+                               {"$group" : {"_id": "$id", "text" : {"$push": "$text_preprocessed"}}}])
+
+
+alltext = []
+for doc in cursor:
+    #print(doc["text"])
+    alltext.append(doc["text"])
+
+
+# Convert list of list to list
+txtlist = [item for items in alltext for item in items]
+
+# Convert list to string
+long_string = ''.join(txtlist)
 
 # Create a WordCloud object
 wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
@@ -18,10 +33,42 @@ wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3,
 wordcloud.generate(long_string)
 
 # Save the word cloud
-wordcloud.to_file("img/wordcloud.png")
+wordcloud.to_file("../img/wordcloud2.png")
 
 # Display the generated image:
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
 
+######################################
+stop = timeit.default_timer()
+print('Time: %.5f' % (stop - start))
+######################################
+
+
+
+
+
+
+
+#2nd method, same result
+###################################################################################
+'''tweet_texts = collection.distinct("text_preprocessed")
+
+# Join the different processed titles together.
+long_string = ','.join(list(tweet_texts))
+
+print(long_string)
+# Create a WordCloud object
+wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
+
+# Generate a word cloud
+wordcloud.generate(long_string)
+
+# Save the word cloud
+wordcloud.to_file("../img/wordcloud2.png")
+
+# Display the generated image:
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()'''
--- FileAlexia/Data analysis and visualization/group_by_country.py
+++ FileAlexia/Data analysis and visualization/group_by_country.py
@@ -1,20 +1,29 @@
-from mongo_db import *
+from FileAlexia.mongodb_scripts.mongo_db import *
+import timeit
 
+
+######################################
+start = timeit.default_timer()
+######################################
 
 collection = db['covidtemp']
 
 
-countries_qty_cursor = collection.aggregate([
-    {"$group":{"_id":"$country", "count":{"$sum":1}}},
-    {"$sort":{"count":-1}}],
-    allowDiskUse=True)
+cursor = collection.aggregate([#{"$limit": 1000}, #in case we want to limit to the first N records
+                                {"$group":{"_id":"$location", "count":{"$sum":1}}},
+                                {"$sort":{"count":-1}}],
+                                allowDiskUse=True)
 
 
 #create dictionary, in case we need it
-countries_qty_dict = {}
-for pair in countries_qty_cursor:
-    print(pair)
-    countries_qty_dict.update({pair['_id']:pair['count']})
+countries_dict = {}
+for doc in cursor:
+    print(doc)
+    countries_dict.update({doc['_id']:doc['count']})
 
-#print(countries_qty_dict)
+#print(countries_dict)
 
+######################################
+stop = timeit.default_timer()
+print('Time: %.5f' % (stop - start))
+######################################
