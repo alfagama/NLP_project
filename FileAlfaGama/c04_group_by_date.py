1b15fd630bdcdfe37f7ccc0eac2253ef30406aa3
Arampatzis Giorgos <48099515+alfagama@users.noreply.github.com>
2020-12-30 17:23:27 +0200

    Update c04_group_by_date.py
    20201230 - new group_by_dates in file

--- FileAlfaGama/c04_group_by_date.py
+++ FileAlfaGama/c04_group_by_date.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from datetime import datetime
 from collections import Counter
+import json
 
 #####################################################################
 connection = MongoClient("mongodb://localhost:27017/")
@@ -10,14 +11,19 @@ collections = db.collection_names()
 
 #####################################################################
 all_dates = []
+count = 0
 for collection in collections:
     tweets = db[collection].find().batch_size(10)
-    if collection == 'vaccine':
+    if collection == 'vaccine_test4':
         for tweet in tweets:
-            date = db[collection].find_one({'created_at': tweet["created_at"]})["created_at"]
-            new_datetime = datetime.strftime(datetime.strptime(date, '%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d')
-            all_dates.append(new_datetime)
-            print(new_datetime)
+            # date = db[collection].find_one({'created_at': tweet["created_at"]})["created_at"]
+            # new_datetime = datetime.strftime(datetime.strptime(date, '%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d')
+            # all_dates.append(new_datetime)
+            # print(new_datetime)
+            date = db[collection].find_one({'tweet_date': tweet["tweet_date"]})["tweet_date"]
+            all_dates.append(date)
+            count += 1
+            print(count)
 
 #####################################################################
 # unique_dates = Counter(all_dates).keys()
@@ -25,4 +31,8 @@ for collection in collections:
 
 unique_dates_frequency = Counter(all_dates)
 print(unique_dates_frequency.items())
+#####################################################################
+with open('Dates/dates.txt'', 'w') as file:
+    file.write(json.dumps(unique_dates_frequency.items()))
+
 #####################################################################
