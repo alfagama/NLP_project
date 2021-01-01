2424aafeb5afeb61050978b9e0781e75d3c43b30
Eleni Tsiolaki <74108347+eleni-tsiolaki@users.noreply.github.com>
2021-01-01 18:05:50 +0200

    Update world_map.py

--- FileEleni/world_map.py
+++ FileEleni/world_map.py
@@ -1,3 +1,4 @@
+#https://plotly.com/python/choropleth-maps/
 from pycountry_convert import country_alpha2_to_continent_code, country_name_to_country_alpha2
 import geopy
 from geopy.geocoders import Nominatim
@@ -9,7 +10,11 @@ import folium
 from folium.plugins import MarkerCluster
 import branca.colormap as cmp
 from folium.features import DivIcon
+from geopy.exc import GeocoderServiceError
 from geopy.exc import GeocoderTimedOut
+from geopy.extra.rate_limiter import RateLimiter
+import plotly
+import plotly.express as px
 
 def do_geocode(address, attempt=1, max_attempts=5):
     try:
@@ -60,15 +65,16 @@ print(unique_tweets_frequency)
 df = []
 listOfContinents = []
 for index, value in enumerate(Counter(all_countries)):
-    print(index)
-    print(value)
+    #print(index)
+    #print(value)
     df.append([value, Counter(all_countries)[value]])
     listOfContinents.append(get_continent(value))
-df_covid = pd.DataFrame(df, columns = ['Country', 'Total Tweets'])
+df_covid = pd.DataFrame(df, columns = ['country', 'counts'])
 print(listOfContinents)
 
 
 geolocator = Nominatim(user_agent="my_user_agent")
+geopy.geocoders.options.default_timeout = 20
 
 #empty map
 world_map= folium.Map(tiles="cartodbpositron")
@@ -85,33 +91,37 @@ step = cmp.StepColormap(
  caption='Color Scale for Map'    #Caption for Color scale or Legend
 )
 
+
+counter = 0
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1,max_retries=0)
 #for each coordinate, create circlemarker of user percent
 for index, value in enumerate(Counter(all_countries)):
-    loc = do_geocode(listOfContinents[index][0] + ',' + listOfContinents[index][1])
-    print(loc)
-    if loc != None:
-        numberOfTweets = Counter(all_countries)[value]
-        lat = loc.latitude
-        long = loc.longitude
-        radius=10
-        popup_text = """Country : {}<br>
-                    #of Tweets : {}<br>"""
-        popup_text = popup_text.format(value,numberOfTweets)
-        folium.map.Marker(location=[lat, long], radius=10,
-                          icon=DivIcon(
-                              icon_size=(40, 40),
-                              icon_anchor=(0, 0),
-                              html='<div style="font-size: 20pt">' + str(numberOfTweets) + '</div>',
-                          )
-                          ).add_to(world_map)
-        #folium.CircleMarker(location = [lat, long], radius=radius, popup= popup_text, fill =True).add_to(marker_cluster)
-    else:
-        print("I got None for this country: ", value)
-
-
-
-
-
+    if counter < 100000:
+        print(value)
+        try:
+            loc = geolocator.geocode(listOfContinents[index][0] + ',' + listOfContinents[index][1])
+            counter = counter + 1
+            print(loc)
+            if loc != None:
+                numberOfTweets = Counter(all_countries)[value]
+                lat = loc.latitude
+                long = loc.longitude
+                radius=10
+                popup_text = """Country : {}<br>
+                            #of Tweets : {}<br>"""
+                popup_text = popup_text.format(value,numberOfTweets)
+                folium.map.Marker(location=[lat, long], radius=1,
+                                  icon=DivIcon(
+                                      icon_size=(20, 20),
+                                      icon_anchor=(0, 0),
+                                      html='<div style="font-size: 10pt">' + str(numberOfTweets) + '</div>',
+                                  )
+                                  ).add_to(world_map)
+                #folium.CircleMarker(location = [lat, long], radius=radius, popup= popup_text, fill =True).add_to(marker_cluster)
+            else:
+                print("I got None for this country: ", value)
+        except GeocoderServiceError:
+            print("to efaga")
 
 folium.Choropleth(
         # The GeoJSON data to represent the world country
@@ -119,7 +129,7 @@ folium.Choropleth(
         data=df_covid,
         name='choropleth COVID-19',
         # The column aceppting list with 2 value; The country name and  the numerical value
-        columns=['Country', 'Total Tweets'],
+        columns=['country', 'counts'],
         key_on='feature.properties.name',
         fill_color='PuRd',
         nan_fill_color='white',
@@ -139,3 +149,16 @@ folium.Choropleth(
 # save map to html file
 step.add_to(world_map)
 world_map.save('index.html')
+
+
+gapminder = px.data.gapminder().query("year==2007")
+df_countries = pd.DataFrame(gapminder)
+df_merged = pd.merge(df_countries, df_covid, on='country')
+
+fig = px.choropleth(df_merged, locations="iso_alpha",
+                    color="counts",
+                    hover_name="country",
+                    color_continuous_scale=px.colors.sequential.Plasma)
+
+plotly.offline.plot(fig, filename='worldmap.html')
+fig.show()
