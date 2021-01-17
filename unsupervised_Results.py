a1fa87b7feed614d8f40413228d064a43fa7917b
Arampatzis Giorgos <48099515+alfagama@users.noreply.github.com>
2021-01-17 17:11:56 +0200

    Update unsupervised_Results.py

--- unsupervised_Results.py
+++ unsupervised_Results.py
@@ -2,11 +2,11 @@ from pymongo import MongoClient
 import pandas as pd
 
 def findLabelPosition(label):
-    if label == 'Positive':
+    if label == '1' or label == 1:
         x = 0
-    elif label == 'Negative':
+    elif label == '-1' or label == -0:
         x = 1
-    else:
+    elif label == '0':
         x = 2
     return x
 
@@ -17,6 +17,7 @@ def createFinalDF(dataframe):
 
     for i, j in dataframe.iterrows():
         print(j[1])
+        # printing the third element of the column
         i = 0
         for x in j[1]:
             if i == 0:
@@ -40,7 +41,7 @@ collections = db.collection_names()
 #####################################################################
 dictVader = {}
 dictTextblob = {}
-dictBilstm = {}
+dictBiLSTM = {}
 docCount = 0
 errors = 0
 for collection in collections:
@@ -49,21 +50,21 @@ for collection in collections:
         for tweet in tweets:
 
             location = tweet["location"]
-            vader = tweet["Vader"]
-            textblob = tweet["TextBlob"]
+            vader = tweet["vader_preprocessed_label"]
+            textblob = tweet["textblob_preprocessed_label"]
             bilstm = tweet["BiLSTM_label"]
 
             dictVader.setdefault(location, [0,0,0])
             dictTextblob.setdefault(location, [0,0,0])
-            dictBilstm.setdefault(location, [0, 0, 0])
+            dictBiLSTM.setdefault(location, [0,0,0])
 
             x_Vader = findLabelPosition(vader)
             x_TextBlob = findLabelPosition(textblob)
-            x_Bilstm = findLabelPosition(bilstm)
+            x_bilstm = findLabelPosition(bilstm)
 
             dictVader[location][x_Vader] += 1
             dictTextblob[location][x_TextBlob] += 1
-            dictBilstm[location][x_Bilstm] += 1
+            dictBiLSTM[location][x_bilstm] += 1
 
             docCount += 1
             print("Count documents ", docCount)
@@ -74,7 +75,7 @@ for collection in collections:
 
 vaderDF = pd.DataFrame(dictVader.items(), columns=['Country', 'Labels'])
 textblobDF = pd.DataFrame(dictTextblob.items(), columns=['Country', 'Labels'])
-bilstmDF = pd.DataFrame(dictBilstm.items(), columns=['Country', 'Labels'])
+bilstmDF = pd.DataFrame(dictBiLSTM.items(), columns=['Country', 'Labels'])
 
 vaderFinal = createFinalDF(vaderDF)
 textblobFinal = createFinalDF(textblobDF)
@@ -88,7 +89,8 @@ bilstmFinal = createFinalDF(bilstmDF)
 # Save Vader and TextBlob results in CSV
 #------------------------------------
 
-vaderFinal.to_csv("quarantine_vader_results.csv")
-textblobFinal.to_csv("quarantine_textblob_results.csv")
-bilstmFinal.to_csv("quarantine_bilstm_results.csv")
+vaderFinal.to_csv("vader_results.csv")
+textblobFinal.to_csv("textblob_results.csv")
+bilstmFinal.to_csv("bilstm_results.csv")
+
 #------------------------------------
