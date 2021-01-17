906b047410d2523916c782c4db2a83ad20800831
Alexia Fytili <33041542+alexiafy@users.noreply.github.com>
2021-01-17 11:20:57 +0200

    Update LSTM_glove.py

--- LSTM/LSTM_glove.py
+++ LSTM/LSTM_glove.py
@@ -1,4 +1,4 @@
-﻿import keras
+import keras
 import tensorflow
 import pymongo
 import pandas as pd
@@ -11,7 +11,7 @@ from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from keras.utils.np_utils import to_categorical
 from keras.callbacks import ModelCheckpoint
-from keras.metrics import Precision, Recall
+#from keras.metrics import Precision, Recall
 import joblib
 from numpy import zeros
 import matplotlib.pyplot as plt
@@ -21,7 +21,7 @@ import pickle
 from sklearn import metrics
 
 
-COLLECTIONAME = 'sentiment140'
+COLLECTIONAME = 'sentiment140_sm'
 
 
 #MongoDB
@@ -71,9 +71,9 @@ print(X_test.shape, Y_test.shape)
 # with a higher number as having more significance. to_categorical is a quick way of encoding the data.
 #[0. 1.] -> positive -> 4
 #[1. 0.] -> negative -> 0
-Y_test_classes_for_evaluation = Y_test.astype(int)
-Y_train = to_categorical(Y_train, num_classes=2)
-Y_test = to_categorical(Y_test, num_classes=2)
+#Y_test_classes_for_evaluation = Y_test.astype(int)
+#Y_train = to_categorical(Y_train, num_classes=2)
+#Y_test = to_categorical(Y_test, num_classes=2)
 #----------------------------------
 
 
@@ -161,52 +161,56 @@ for word, i in tokenizer.word_index.items():
 # LSTM model
 #----------------------------------
 model = Sequential()
-model.add(Embedding(input_dim=vocab_size, output_dim=EMBEDDING_DIM, weights=[embedding_matrix], input_length = X_train.shape[1], trainable=True))#, input_length=4
+model.add(Embedding(input_dim=vocab_size, output_dim=EMBEDDING_DIM, weights=[embedding_matrix], input_length=X_train.shape[1], trainable=True))#, input_length=4
 
-model.add(SpatialDropout1D(0.4))
+model.add(SpatialDropout1D(0.5))
 model.add(LSTM(30, return_sequences=True, recurrent_dropout=0.5))
-model.add(LSTM(30, recurrent_dropout=0.5))
+model.add(LSTM(30, dropout=0.5, recurrent_dropout=0.5))
 model.add(Dense(30, activation='sigmoid'))
-model.add(Dense(2, activation='softmax'))
-
+model.add(Dense(1, activation='sigmoid'))
 
 optimizer = Adam(lr=0.01, decay=0.001)
-model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy', Precision(), Recall()]) #binary_crossentropy for binary classification, categorical_crossentropy for multiclass
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy']) #, Precision(), Recall()
+
 print(model.summary())
 #----------------------------------
 
 
 # Fit model
 #----------------------------------
-batch_size = 32
+batch_size = 128
 checkpoint1 = ModelCheckpoint("weights/LSTM_glove_best_model1.hdf5", monitor='val_accuracy', verbose=1, save_best_only=True, mode='auto', period=1,save_weights_only=False)
-history = model.fit(X_train, Y_train, epochs=8, validation_split=0.2, callbacks=[checkpoint1], batch_size=batch_size) #validation_data=(X_test, Y_test),
+history = model.fit(X_train, Y_train, epochs=10, validation_split=0.2, callbacks=[checkpoint1], batch_size=batch_size) #validation_data=(X_test, Y_test),
 #----------------------------------
 
 
 
 # Evaluate with Keras
 #----------------------------------
-loss, accuracy, precision, recall = model.evaluate(X_test, Y_test, verbose=0, batch_size=batch_size)
+loss, accuracy = model.evaluate(X_test, Y_test, verbose=0, batch_size=batch_size) #, precision, recall
 #print(model.metrics_names)
 print("Evaluation on test data using Keras metrics:")
 print("Loss: %.6f" % (loss))
 print("Accuracy: %.6f" % (accuracy))
-print("Precision: %.6f" % (precision))
-print("Recall: %.6f" % (recall))
+#print("Precision: %.6f" % (precision))
+#print("Recall: %.6f" % (recall))
 #----------------------------------
 
 
 
 #Evaluation with Sklearn
 #----------------------------------
+#y_pred = model.predict_classes(X_test, verbose=0)
+
 y_pred = model.predict_classes(X_test, verbose=0)
+Y_test = Y_test.astype(int)
+
 
 print("\nEvaluation on test data using Sklearn metrics:")
-print("Accuracy: %.6f" % metrics.accuracy_score(Y_test_classes_for_evaluation, y_pred))
-print("Precision: %.6f" % metrics.precision_score(Y_test_classes_for_evaluation, y_pred, average='macro'))#, labels=np.unique(y_pred)))#, labels=np.unique(y_predicted)
-print("Recall: %.6f" % metrics.recall_score(Y_test_classes_for_evaluation, y_pred, average='macro'))
-print("F1: %.6f \n" % metrics.f1_score(Y_test_classes_for_evaluation, y_pred, average='macro'))
+print("Accuracy: %.6f" % metrics.accuracy_score(Y_test, y_pred))
+print("Precision: %.6f" % metrics.precision_score(Y_test, y_pred, average='macro'))#, labels=np.unique(y_pred)))#, labels=np.unique(y_predicted)
+print("Recall: %.6f" % metrics.recall_score(Y_test, y_pred, average='macro'))
+print("F1: %.6f \n" % metrics.f1_score(Y_test, y_pred, average='macro'))
 #----------------------------------
 
 
@@ -218,12 +222,12 @@ b = pad_sequences(b, maxlen=max_length)
 print(model.predict(b))'''
 #----------------------------------
 
-
+folder = 'LSTM-glove-binary-trainableTrue-noPrecision'
 
 # Save LSTM model
 #----------------------------------
 print("Saving LSTM model to disk")
-model.save('models/LSTM_glove_model.h5')
+model.save('models/'+ folder +'/LSTM_glove_model.h5')
 #----------------------------------
 
 
@@ -231,10 +235,19 @@ model.save('models/LSTM_glove_model.h5')
 # Save tokenizer
 #----------------------------------
 print("Saving tokenizer to disk")
-with open('models/LSTM_glove_tokenizer.pickle', 'wb') as handle:
+with open('models/'+ folder +'/LSTM_glove_tokenizer.pickle', 'wb') as handle:
     pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 #----------------------------------
 
+
+
+# Serialize model to JSON
+#----------------------------------
+print("Saving JSON to disk")
+model_json = model.to_json()
+with open('models/'+ folder +'/LSTM_glove_model_JSON.pickle', "w") as json_file:
+    json_file.write(model_json)
+#----------------------------------
 
 
 # Plot accuracy and loss
@@ -246,7 +259,7 @@ plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper left')
-plt.savefig("plots/LSTM_glove_accuracy.png")
+plt.savefig("plots/"+ folder +"/LSTM_glove_accuracy.png")
 plt.show()
 
 
@@ -258,7 +271,7 @@ plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper left')
-plt.savefig("plots/LSTM_glove_loss.png")
+plt.savefig("plots/"+ folder +"/LSTM_glove_loss.png")
 plt.show()
 #----------------------------------
 
