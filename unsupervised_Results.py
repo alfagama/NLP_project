b83b83ae5e914fc547b07e212fe64bb2ee889a4d
Eleni Tsiolaki <74108347+eleni-tsiolaki@users.noreply.github.com>
2021-01-17 16:55:22 +0200

    Update unsupervised_Results.py

--- unsupervised_Results.py
+++ unsupervised_Results.py
@@ -17,7 +17,6 @@ def createFinalDF(dataframe):
 
     for i, j in dataframe.iterrows():
         print(j[1])
-        # printing the third element of the column
         i = 0
         for x in j[1]:
             if i == 0:
@@ -41,6 +40,7 @@ collections = db.collection_names()
 #####################################################################
 dictVader = {}
 dictTextblob = {}
+dictBilstm = {}
 docCount = 0
 errors = 0
 for collection in collections:
@@ -51,15 +51,19 @@ for collection in collections:
             location = tweet["location"]
             vader = tweet["Vader"]
             textblob = tweet["TextBlob"]
+            bilstm = tweet["BiLSTM_label"]
 
             dictVader.setdefault(location, [0,0,0])
             dictTextblob.setdefault(location, [0,0,0])
+            dictBilstm.setdefault(location, [0, 0, 0])
 
             x_Vader = findLabelPosition(vader)
             x_TextBlob = findLabelPosition(textblob)
+            x_Bilstm = findLabelPosition(bilstm)
 
             dictVader[location][x_Vader] += 1
             dictTextblob[location][x_TextBlob] += 1
+            dictBilstm[location][x_Bilstm] += 1
 
             docCount += 1
             print("Count documents ", docCount)
@@ -70,9 +74,11 @@ for collection in collections:
 
 vaderDF = pd.DataFrame(dictVader.items(), columns=['Country', 'Labels'])
 textblobDF = pd.DataFrame(dictTextblob.items(), columns=['Country', 'Labels'])
+bilstmDF = pd.DataFrame(dictBilstm.items(), columns=['Country', 'Labels'])
 
 vaderFinal = createFinalDF(vaderDF)
 textblobFinal = createFinalDF(textblobDF)
+bilstmFinal = createFinalDF(bilstmDF)
 
 #---------------------------------------------------------------------
 
@@ -84,5 +90,5 @@ textblobFinal = createFinalDF(textblobDF)
 
 vaderFinal.to_csv("quarantine_vader_results.csv")
 textblobFinal.to_csv("quarantine_textblob_results.csv")
-
+bilstmFinal.to_csv("quarantine_bilstm_results.csv")
 #------------------------------------
