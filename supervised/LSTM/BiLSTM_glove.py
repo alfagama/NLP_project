8de22c6781de61ee23c1e052f1b2a2c2d9ea4946
Alexia Fytili <33041542+alexiafy@users.noreply.github.com>
2021-02-03 12:02:06 +0200

    Update BiLSTM_glove.py

--- supervised/LSTM/BiLSTM_glove.py
+++ supervised/LSTM/BiLSTM_glove.py
@@ -71,9 +71,9 @@ print(X_test.shape, Y_test.shape)
 # with a higher number as having more significance. to_categorical is a quick way of encoding the data.
 #[0. 1.] -> positive -> 4
 #[1. 0.] -> negative -> 0
-Y_test_classes_for_evaluation = Y_test.astype(int)
-Y_train = to_categorical(Y_train, num_classes=2)
-Y_test = to_categorical(Y_test, num_classes=2)
+#Y_test_classes_for_evaluation = Y_test.astype(int)
+#Y_train = to_categorical(Y_train, num_classes=2)
+#Y_test = to_categorical(Y_test, num_classes=2)
 #----------------------------------
 
 
@@ -166,18 +166,16 @@ model.add(Embedding(input_dim=vocab_size, output_dim=EMBEDDING_DIM, weights=[emb
 model.add(SpatialDropout1D(0.4))
 model.add(Bidirectional(LSTM(30, dropout=0.2, recurrent_dropout=0.2)))
 model.add(Dense(30, activation='sigmoid'))
-model.add(Dense(2, activation='softmax'))
+model.add(Dense(1, activation='sigmoid'))
 
-
-optimizer = Adam(lr=0.01, decay=0.001)
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy']) #, Precision(), Recall() #binary_crossentropy for binary classification, categorical_crossentropy for multiclass
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy']) #, Precision(), Recall() #binary_crossentropy for binary classification, categorical_crossentropy for multiclass
 print(model.summary())
 #----------------------------------
 
 
 # Fit model
 #----------------------------------
-batch_size = 32
+batch_size = 128
 checkpoint1 = ModelCheckpoint("weights/BiLSTM_glove_best_model1.hdf5", monitor='val_accuracy', verbose=1, save_best_only=True, mode='auto', period=1,save_weights_only=False)
 history = model.fit(X_train, Y_train, epochs=10, validation_split=0.2, callbacks=[checkpoint1], batch_size=batch_size) #validation_data=(X_test, Y_test),
 #----------------------------------
@@ -200,12 +198,13 @@ print("Recall: %.6f" % (recall))'''
 #Evaluation with Sklearn
 #----------------------------------
 y_pred = model.predict_classes(X_test, verbose=0)
+Y_test = Y_test.astype(int)
 
 print("\nEvaluation on test data using Sklearn metrics:")
-print("Accuracy: %.6f" % metrics.accuracy_score(Y_test_classes_for_evaluation, y_pred))
-print("Precision: %.6f" % metrics.precision_score(Y_test_classes_for_evaluation, y_pred, average='macro'))#, labels=np.unique(y_pred)))#, labels=np.unique(y_predicted)
-print("Recall: %.6f" % metrics.recall_score(Y_test_classes_for_evaluation, y_pred, average='macro'))
-print("F1: %.6f \n" % metrics.f1_score(Y_test_classes_for_evaluation, y_pred, average='macro'))
+print("Accuracy: %.6f" % metrics.accuracy_score(Y_test, y_pred))
+print("Precision: %.6f" % metrics.precision_score(Y_test, y_pred, average='macro'))#, labels=np.unique(y_pred)))#, labels=np.unique(y_predicted)
+print("Recall: %.6f" % metrics.recall_score(Y_test, y_pred, average='macro'))
+print("F1: %.6f \n" % metrics.f1_score(Y_test, y_pred, average='macro'))
 #----------------------------------
 
 
